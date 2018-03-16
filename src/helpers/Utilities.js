03df7d3dea64e00cee96aff6981eee257458cf6c
dmkitui <daniel.kitui@andela.com>
2018-03-16 11:15:03 +0300

    Add method to evaluate token status

--- src/helpers/Utilities.js
+++ src/helpers/Utilities.js
@@ -1,7 +1,22 @@
-import React, { Component } from 'react';
 import moment from 'moment';
+import decode from 'jwt-decode';
+import ModalDialogs from './Dialogs';
 
-export default function dateTimeFormat(timeObj) {
-    let time = new Date(timeObj + ' UTC');
-    return moment(time.getTime()).format('MMM Do YYYY, [at] h:mm[]a');
-}
+const Helpers = {
+  dateTimeFormat(timeObj) {
+    const time = new Date(`${timeObj} UTC`);
+    return moment(time.getTime()).format('MMM Do YYYY, [at] h:mm[ ]a');
+  },
+  isTokenValid() {
+    const token = localStorage.getItem('token');
+    try {
+      const decoded = decode(token);
+      return (decoded.exp > (Date.now() / 1000))
+    } catch (error) {
+      ModalDialogs.error('You are not logged in. Log in first.');
+      return false;
+    }
+  },
+};
+
+export default Helpers;
